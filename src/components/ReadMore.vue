f007adecb19d6a6bf8577614466c1d820bb6c172
Enol-Igareta <enol1code@gmail.com>
2023-01-06 17:17:00 +0100

    Añadiendo funcionalidad read more

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -2,14 +2,18 @@
 import { ref } from "vue";
 
 const title = ref("Exercise Read More");
-
+let active = ref(true);
+const addClass =  ()  => {
+  active.value = active.value === true ? false : true
+  console.log(active.value)
+};
 </script>
 
 <template>
   <div class="container text-center">
     <h1 class="my-3">{{ title }}</h1>
-    <div>
-      <p class="mb-3 d-block cut-text">
+    <div class="div">
+      <p class="mb-3 d-block" :class="{'cut-text': active}" >
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
         ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
         molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
@@ -17,10 +21,11 @@ const title = ref("Exercise Read More");
         temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
         quaerat autem non hic, quidem dolore laboriosam.
       </p>
-      <button class="btn btn-primary">Show More</button>
+      <button class="btn btn-primary" @click="addClass">Show More</button>
     </div>
     <hr class="mb-4" />
   </div>
 </template>
 
-<style lang="css"></style>
+<style lang="css">
+</style>
